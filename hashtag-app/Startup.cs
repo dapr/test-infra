4358b99fe2c836174dd5be4d5d0dc5c098b1197f
Young Bu Park <youngp@microsoft.com>
2020-03-27 16:10:29 -0700

    Add Hashtag counter controller and app (#37)
    * Add HashTag Counter controller
    * Add hashtag counter app to k8s deployment

--- hashtag-app/Startup.cs
+++ hashtag-app/Startup.cs
@@ -5,10 +5,6 @@
 
 namespace Dapr.Tests.HashTagApp
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.HttpsPolicy;
@@ -36,21 +32,14 @@ namespace Dapr.Tests.HashTagApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.EnvironmentName.Contains(".dev"))
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
         }
     }
 }

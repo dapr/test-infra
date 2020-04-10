1429fcb38a57435b4943dfde7993bb5082502049
LM <lemai>
2020-04-09 18:00:55 -0700

    Add a get method to the actor interface and add snapshot app which queries it

---
+++ snapshot/Constants.cs
@@ -0,0 +1,12 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+namespace Dapr.Tests.Snapshot
+{
+    public class AppSettings
+    {        
+        public const string DaprHTTPAppPort = "DaprHTTPAppPort";        
+    }
+}

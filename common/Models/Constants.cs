bb3f9730b5630b3cef2981461933eb9432e23b96
lolorol <lalesle@microsoft.com>
2020-05-13 23:25:53 +0800

    #61 Move hashtag and constants to shared library

---
+++ common/Models/Constants.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Dapr.Tests.Common.Models
+{
+    public static class Constants
+    {
+        public static string[] HashTags = new string[]
+        {
+            "circle",
+            "ellipse",
+            "square",
+            "rectangle",
+            "triangle",
+            "star",
+            "cardioid",
+            "epicycloid",
+            "limocon",
+            "hypocycoid"
+        };
+
+        public static string[] Sentiments = new string[]
+        {
+            "verynegative",
+            "negative",
+            "neutral",
+            "positive",
+            "verypositive"
+        };
+    }
+}

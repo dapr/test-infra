da1595d5f68b573d5876c9ccd2e4260908d54ffc
Whit Waldo <whit.waldo@innovian.net>
2025-02-07 10:49:38 -0600

    Workflow fixes (#257)
    * Mild longhaul overhaul
    
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>
    
    * Disabled build warning
    
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>
    
    * Updated dockerfile to use .NET 8
    
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>
    
    ---------
    
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

--- workflow-gen/Startup.cs
+++ workflow-gen/Startup.cs
@@ -1,72 +1,83 @@
-﻿// ------------------------------------------------------------
+﻿// ------------------------------------------------------------------------
+// Copyright 2025 The Dapr Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 // ------------------------------------------------------------
 
-namespace WorkflowGen
+namespace WorkflowGen;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Text.Json;
+
+/// <summary>
+/// Startup class.  This is a stub and being left in case we want to modify this 
+/// app to start/stop sending messages on receiving a call on an endpoint.
+/// </summary>
+public class Startup
 {
-    using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Hosting;
-    using System.Text.Json;
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Startup"/> class.
+    /// </summary>
+    /// <param name="configuration">Configuration.</param>
+    public Startup(IConfiguration configuration)
+    {
+        this.Configuration = configuration;
+    }
 
     /// <summary>
-    /// Startup class.  This is a stub and being left in case we want to modify this 
-    /// app to start/stop sending messages on receiving a call on an endpoint.
+    /// Gets the configuration.
     /// </summary>
-    public class Startup
+    public IConfiguration Configuration { get; }
+
+    /// <summary>
+    /// Configures Services.
+    /// </summary>
+    /// <param name="services">Service Collection.</param>
+    public void ConfigureServices(IServiceCollection services)
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Startup"/> class.
-        /// </summary>
-        /// <param name="configuration">Configuration.</param>
-        public Startup(IConfiguration configuration)
+        services.AddDaprClient();
+
+        services.AddSingleton(new JsonSerializerOptions()
         {
-            this.Configuration = configuration;
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            PropertyNameCaseInsensitive = true,
+        });
+    }     
+
+    /// <summary>
+    /// Configures Application Builder and WebHost environment.
+    /// </summary>
+    /// <param name="app">Application builder.</param>
+    /// <param name="env">Webhost environment.</param>
+    /// <param name="serializerOptions">Options for JSON serialization.</param>
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, JsonSerializerOptions serializerOptions)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
         }
 
-        /// <summary>
-        /// Gets the configuration.
-        /// </summary>
-        public IConfiguration Configuration { get; }
+        app.UseRouting();
 
-        /// <summary>
-        /// Configures Services.
-        /// </summary>
-        /// <param name="services">Service Collection.</param>
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDaprClient();
+        app.UseCloudEvents();
 
-            services.AddSingleton(new JsonSerializerOptions()
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                PropertyNameCaseInsensitive = true,
-            });
-        }     
-
-        /// <summary>
-        /// Configures Application Builder and WebHost environment.
-        /// </summary>
-        /// <param name="app">Application builder.</param>
-        /// <param name="env">Webhost environment.</param>
-        /// <param name="serializerOptions">Options for JSON serialization.</param>
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, JsonSerializerOptions serializerOptions)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseCloudEvents();
-
-            app.UseEndpoints(endpoints =>
-            {       
-            });
-        }
+        app.UseEndpoints(endpoints =>
+        {       
+        });
     }
 }
